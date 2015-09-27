cb0b816b488ff5ba6fa72dc6d22b4b242745a0c2
Eduardo Romero <eduardodrh@gmail.com>
2015-09-27 01:55:09 -0500

    Primera version, sin checar

---
+++ Alumno.java
@@ -0,0 +1,65 @@
+class Alumno {
+	
+//ATRIBUTOS
+	private int clave;
+	private String nom;	
+	private String genero;	
+	private Calificaciones calif;
+	
+//CONSTRUCTORES
+	public Alumno(){
+		calif = new Calificaciones(); //se crea el objeto 
+	}
+	
+//METODOS
+	public void CapturaAlumno(){
+		System.out.println("Captura de Datos Personales:\n");
+		LeeClave();
+		LeeNom();
+		LeeGenero();
+		calif.CapturaCalificaciones();
+	}
+	
+	public void LeeClave(){
+		clave = Teclado.LeeEntero("# de cuenta: ");
+	}
+
+	public void LeeNom(){
+		nom = Teclado.LeeCadena("Nombre: ");
+	}
+
+	public void LeeGenero(){
+		genero = Teclado.LeeCadena("Genero: ");
+	}
+
+	public void Listar(){
+        System.out.print(clave+"\t"+nom+"\t"+genero+"\t");
+        calif.Listar();
+    }
+	
+	public void Actualizar(){
+		int opcion;
+		Menu menu = new Menu();
+		
+		do{
+			opcion = menu.ListaMenu("1)Nombre\n2)Genero\n3)Calificaciones\n4)Salir\nOpcion: ",3);
+		   	switch (opcion) {
+				case 1:
+					LeeNom();
+				break;
+
+				case 2:
+				 	LeeGenero();
+				break;
+
+				case 3:
+					calif.Actualizar();
+				break;
+			}
+		} while (opcion != 3);
+	}
+
+	public int GetClave(){
+		return clave;
+	}
+}
